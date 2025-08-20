e23a2ad0adac4ddb9cb006f5b2f15afb14a5ae27
Deprime <abdulmuizayomide2@gmail.com>
2025-08-20 03:45:08 +0100

    muiz0

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,34 @@ import Withdraw from "./pages/Withdraw";
 import Refer from "./pages/Refer";
 import Profile from "./pages/Profile";
 
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
+// import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
-          <Route path="/" element={<Signup/>}/>
-          <Route path="/register" element={<Signup/>}/>
-          <Route path="/login" element={<Login/>} />
+      <Route path="/" element={<Signup />} />
+      {/* <Route path="/register" element={<Signup />} /> */}
       <Route path="/login" element={<Login />} />
+      {/* <Route path="/login" element={<Login />} /> */}
       <Route path="/signup" element={<Signup />} />
-      <Route path={"/dashboard"} element={<Maindashboard children={<Dashboard />} />} />
-      <Route path={"/history"} element={<Maindashboard children={<History />} />} />
-      <Route path={"/withdraw"} element={<Maindashboard children={<Withdraw />} />} />
+      <Route
+        path={"/dashboard"}
+        element={<Maindashboard children={<Dashboard />} />}
+      />
+      <Route
+        path={"/history"}
+        element={<Maindashboard children={<History />} />}
+      />
+      <Route
+        path={"/withdraw"}
+        element={<Maindashboard children={<Withdraw />} />}
+      />
       <Route path={"/refer"} element={<Maindashboard children={<Refer />} />} />
-      <Route path={"/profile"} element={<Maindashboard children={<Profile />} />} />
+      <Route
+        path={"/profile"}
+        element={<Maindashboard children={<Profile />} />}
+      />
     </Routes>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Form from '../components/nav/form'
+import Edit from './Edit'
 import { LockKeyholeIcon, Mail, User, UserPenIcon } from 'lucide-react'
 import assets from '../assets/assests'
 
@@ -32,7 +32,7 @@ const Signup = () => {
   ]
   return (
     <>
-      <Form image={assets.signUp} fields={fields} formSubject={"user registration"} formText={"lets Start this journey and make money"} otherActions={["Already have an account?","Login"]} btnText={"Register"} formAction={()=>alert("Signed Up successfully")} bg={"black"} imgWid={"70%"} smBg={"../../../public/signup-logo.png"}/>
+      <Edit image={assets.signUp} fields={fields} formSubject={"user registration"} formText={"lets Start this journey and make money"} otherActions={["Already have an account?","Login"]} btnText={"Register"} formAction={()=>alert("Signed Up successfully")} bg={"black"} imgWid={"70%"} smBg={"../../../public/signup-logo.png"}/>
     </>
   )
 }
