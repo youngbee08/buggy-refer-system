c4e8b0696189685dc1cb4beb98e905cdd068e165
youngbee08 <youngbeeh08@gmail.com>
2025-08-21 13:59:34 +0100

    added functionaliy to balance view

--- src/components/WithdrawPopup.jsx
+++ src/components/WithdrawPopup.jsx
@@ -5,7 +5,11 @@ const WithdrawPopup = () => {
     <div className='w-1/4 mx-auto translate-y-[-10rem] bg-secClrWhite p-4 rounded-[7px] flex flex-col gap-5'>
       <h3>Enter your 4-digit PIN</h3>
       <div className="flex flex-col gap-5 items-center">
-        
+        {
+          [Array[4]].map((_,index)=>(
+            <p key={index}>hi</p>
+          ))
+        }
       </div>
     </div>
   )
--- src/pages/Withdraw.jsx
+++ src/pages/Withdraw.jsx
@@ -1,4 +1,4 @@
-import { EyeClosed } from 'lucide-react'
+import { EyeClosed, EyeIcon } from 'lucide-react'
 import React from 'react'
 import { useState } from 'react'
 import WithdrawPopup from '../components/WithdrawPopup';
@@ -6,16 +6,17 @@ import WithdrawPopup from '../components/WithdrawPopup';
 const Withdraw = () => {
   const [toggleWithdraw,setToggleWithdraw] = useState(false);
   const [withdrawPopup,setWithdrawPopup] = useState(false);
+  const [toggleBalance,setToggleBalance] = useState(true);
   return (
     <div className='flex flex-col gap-6 pb-[6rem]'>
       <div className="bg-pryClr flex flex-col w-[90%] lg:w-[95%] mx-auto mt-4 rounded-[10px] p-4">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 cursor-pointer" onClick={()=> setToggleBalance(!toggleBalance)}>
           <h3 className='text-base font-semibold'>Wallet balance</h3>
-          <EyeClosed size={18}/>
+          {toggleBalance ? (<EyeClosed size={18}/>) : (<EyeIcon size={18}/>)}
         </div>
         <div className="flex flex-col gap-2 items-start lg:items-center">
-          <h2 className='text-2xl lg:text-4xl font-bold text-accClrYellow'>N5,000</h2>
-          <button className='bg-secClrWhite text-secClrBlack rounded-[6px] px-7 py-2 text-base lg:text-xl font-semibold' onClick={()=>setToggleWithdraw(!toggleWithdraw)}>Withdraw</button>
+          <h2 className='text-2xl lg:text-4xl font-bold text-accClrYellow'>{toggleBalance ? 'N5,000' : '****'}</h2>
+          <button className='bg-secClrWhite text-secClrBlack cursor-pointer rounded-[6px] px-7 py-2 text-base lg:text-xl font-semibold' onClick={()=>setToggleWithdraw(!toggleWithdraw)}>Withdraw</button>
         </div>
       </div>
       {
